5871f87b4008b5ba345356f0826ea0e9766ad5c9
Marco Fortina <marco_fortina@hotmail.it>
2026-05-03 10:26:04 +0000

    feat: add executable access invariant model

---
+++ internal/formalmodel/model.go
@@ -0,0 +1,86 @@
+package formalmodel
+
+import "fmt"
+
+type State struct {
+	Clients map[string]bool
+	Access  map[AccessKey]bool
+}
+
+type AccessKey struct {
+	ClientID string
+	SecretID string
+	Version  int
+}
+
+func NewState() State {
+	return State{Clients: map[string]bool{}, Access: map[AccessKey]bool{}}
+}
+
+func (s State) AddClient(clientID string) State {
+	next := s.clone()
+	next.Clients[clientID] = true
+	return next
+}
+
+func (s State) RevokeClient(clientID string) State {
+	next := s.clone()
+	next.Clients[clientID] = false
+	for key := range next.Access {
+		if key.ClientID == clientID {
+			delete(next.Access, key)
+		}
+	}
+	return next
+}
+
+func (s State) GrantRead(clientID, secretID string, version int) State {
+	next := s.clone()
+	if next.Clients[clientID] {
+		next.Access[AccessKey{ClientID: clientID, SecretID: secretID, Version: version}] = true
+	}
+	return next
+}
+
+func (s State) RevokeRead(clientID, secretID string, version int) State {
+	next := s.clone()
+	delete(next.Access, AccessKey{ClientID: clientID, SecretID: secretID, Version: version})
+	return next
+}
+
+func (s State) StrongRevokeSecret(secretID string, activeVersion int) State {
+	next := s.clone()
+	for key := range next.Access {
+		if key.SecretID == secretID && key.Version != activeVersion {
+			delete(next.Access, key)
+		}
+	}
+	return next
+}
+
+func (s State) CanRead(clientID, secretID string, version int) bool {
+	return s.Clients[clientID] && s.Access[AccessKey{ClientID: clientID, SecretID: secretID, Version: version}]
+}
+
+func (s State) CheckInvariants() error {
+	for key := range s.Access {
+		if !s.Clients[key.ClientID] {
+			return fmt.Errorf("revoked client %s has access to %s/%d", key.ClientID, key.SecretID, key.Version)
+		}
+		if key.Version <= 0 {
+			return fmt.Errorf("non-positive secret version in access grant: %d", key.Version)
+		}
+	}
+	return nil
+}
+
+func (s State) clone() State {
+	next := NewState()
+	for key, value := range s.Clients {
+		next.Clients[key] = value
+	}
+	for key, value := range s.Access {
+		next.Access[key] = value
+	}
+	return next
+}
